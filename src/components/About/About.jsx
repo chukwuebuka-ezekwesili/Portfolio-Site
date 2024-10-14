f9dfb1768655e72950ed8ba4417e9c3e41ec7f68
ezeks-cb <ebezekwesili@gmail.com>
2024-10-14 07:49:02 -0600

    feat: New Components

---
+++ src/components/About/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          className={styles.aboutImage}
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me Doing a thumbs up"
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img
+              className={styles.azureDevops}
+              src={getImageUrl("about/azureDevops.png")}
+              alt="Devops Icon"
+            />
+            <div className={styles.aboutItemText}>
+              <h3>DeVops Engineer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
