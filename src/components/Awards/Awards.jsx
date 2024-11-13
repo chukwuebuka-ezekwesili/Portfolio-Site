ec76ec5543a71332c04c004598b5eaf189d0039c
ezeks-cb <ebezekwesili@gmail.com>
2024-11-13 14:04:36 -0700

    Made some edits to the components

---
+++ src/components/Awards/Awards.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Awards = () => {
+  return <div>Awards</div>;
+};
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,8 +9,22 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Ebuka</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience in React and
-          NodeJS. Feel free to reach out if you'd like to know more about me!
+          With a solid background of over 2 years in core web technologies
+          including React, Nodejs, Expressjs, website deployment and
+          maintenance. My expertise encompasses react native, stripe
+          integration, virtualization, terraform and automation. Additionally,
+          having earned an Honours degree in Computer Programming and Analysis,
+          I pride myself on honing my skills in planning, developing, testing,
+          and deploying integrated software solutions. Furthermore, I am
+          equipped with current skills in building web and mobile applications
+          (Android and IOS), oracle database design and programming, system
+          designs and architecture, manual and automated software testing
+          (Utilising Katalon Studio), project management, presentation and
+          project documentation skills, technical writing skills and vast
+          knowledge of programming languages (JavaScript, JAVA, Python, etc)
+          with up-to-date expertise in frontend and backend frameworks
+          technologies (Spring boot, Expressjs, Nodejs, Lombok, React, React
+          Native, Flutter).
         </p>
         <a
           href="mailto:cebuka.blessing@gmail.com"
